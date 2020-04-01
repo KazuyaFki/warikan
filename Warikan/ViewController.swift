8bae669d8b9b3c67fed79342f87dff6a139ca12f
KazuyaFki <complete501xx@yahoo.co.jp>
2020-04-01 19:01:42 +0900

    Initial Commit

---
+++ Warikan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Warikan
+//
+//  Created by Kazuya Fukui on 2020/04/01.
+//  Copyright © 2020 Kazuya Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
